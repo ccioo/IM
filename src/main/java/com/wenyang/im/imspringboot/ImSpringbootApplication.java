93da32665b8335fa97c03b9eb20c5d83a35758f2
Yangwen <yangwen@qq.com>
2019-09-04 01:05:25 +0800

    IM项目初始化提交

---
+++ src/main/java/com/wenyang/im/imspringboot/ImSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.wenyang.im.imspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImSpringbootApplication.class, args);
+    }
+
+}
