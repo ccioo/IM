5ef395d204572ce028662cd5bd013b36b466977b
yangwen <yangwen@pdnews.cn>
2019-09-27 19:00:01 +0800

    IM 处理连接收尾

--- src/main/java/com/wenyang/im/rpc/config/IMServerConfig.java
+++ src/main/java/com/wenyang/im/rpc/config/IMServerConfig.java
@@ -1,25 +1,8 @@
-/*
- * Copyright (c) 2012-2017 The original author or authors
- * ------------------------------------------------------
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * and Apache License v2.0 which accompanies this distribution.
- *
- * The Eclipse Public License is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * The Apache License v2.0 is available at
- * http://www.opensource.org/licenses/apache2.0.php
- *
- * You may elect to redistribute this code under either of these licenses.
- */
-
 package com.wenyang.im.rpc.config;
 
+import com.common.core.constants.BrokerConstants;
 
-/**
- * Base interface for all configuration implementations (filesystem, memory or classpath)
- */
+
 public abstract class IMServerConfig {
 
     public static final String DEFAULT_CONFIG = "classpath:wildfirechat.conf";
